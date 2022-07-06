eb29b87d207fadbea43c035a660f9ec072c8c614
Michael Phan <michaelphan35@gmail.com>
2022-07-06 16:20:27 -0700

    basic outline for contact page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,12 @@ import "./contact.scss"
 export default function contact() {
   return (
     <div className='contact' id='contact'>
-
+      <h1>Contact</h1>
+      <h2>Please reach out to me via:</h2>
+        <ul>
+          <li>Email: michaelphan35@gmail.com</li>
+          <li>PhoneL 480-745-0146</li>
+        </ul>
     </div>
   )
 }
