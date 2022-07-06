3e4e6b24a89c57d711aeb956fedc90363838cc8e
Michael Phan <michaelphan35@gmail.com>
2022-07-06 14:25:53 -0700

    worked on intro component

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,17 @@
 import "./navbar.scss"
-
 export default function navbar() {
   return (
     <div className="navbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">Zekkan</a>
+          <div className="itemContainer">
+            
+          </div>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
