e8359c8a14f84d3cb01614ebc7ed81e34814372d
Michael Phan <michaelphan35@gmail.com>
2022-07-06 16:17:32 -0700

    outline for portfolio-linked to github

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -6,31 +6,31 @@ export default function portfolio() {
         <h1>Portfolio</h1>
         <ul>
           <li className="active">Run Buddy</li>
-          <li>Now Spinng</li>
-          <li>Spotify Music Search</li>
-          <li>Social API</li>
-          <li>E-Commerce</li>
+          <li className="active">Web Dev Social</li>
+          <li className="active">Spotify Music Search</li>
+          <li className="active">Social API</li>
+          <li className="active">E-Commerce</li>
         </ul>
         <div className="container">
           <div className="item">
-            <img src="images/placeholder-image.png" alt="" />
-            <h3>hello</h3>
+            <a href="https://github.com/michaelphan35/run-buddy"><img src="images/placeholder-image.png" alt=""/></a>
+            <h3>Run Buddy</h3>
           </div>
           <div className="item">
-            <img src="images/placeholder-image.png" alt="" />
-            <h3>hello</h3>
+            <a href="https://github.com/michaelphan35/web-dev-social"><img src="images/placeholder-image.png" alt=""/></a>
+            <h3>Web Dev Social</h3>
           </div>
           <div className="item">
-            <img src="images/placeholder-image.png" alt="" />
-            <h3>hello</h3>
+            <a href="/"><img src="images/Capture1.JPG" alt=""/></a>
+            <h3>Spotify Music Search</h3>
           </div>
           <div className="item">
-            <img src="images/placeholder-image.png" alt="" />
-            <h3>hello</h3>
+            <a href="https://github.com/michaelphan35/social-api"><img src="images/placeholder-image.png" alt=""/></a>
+            <h3>Social Api</h3>
           </div>
           <div className="item">
-            <img src="images/placeholder-image.png" alt="" />
-            <h3>hello</h3>
+            <a href="https://github.com/michaelphan35/e-commerce"><img src="images/placeholder-image.png" alt=""/></a>
+            <h3>E-Commerce</h3>
           </div>
         </div>
     </div>
