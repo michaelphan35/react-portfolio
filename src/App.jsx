1f0153d2a632ba4c0104439b60cdc6c4de67ba5c
Michael Phan <michaelphan35@gmail.com>
2022-07-06 15:24:12 -0700

    finished nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Portfolio from "./components/portfolio/portfolio";
 import Contact from "./components/contact/contact";
 import"./app.scss";
 import { useState } from "react";
+import Menu from "./components/menu/menu";
 
 function App() {
   const [menuOpen , setMenuOpen] = useState(false)
   return (
     <div className="app">
       <Navbar menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen = {menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
