c4f38a63fe671a4a8c9614274c14fef640de14d5
Michael Phan <michaelphan35@gmail.com>
2022-07-19 05:15:40 -0700

    finished contacts

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,14 +1,48 @@
-import "./contact.scss"
+import "./contact.scss";
+import PDF from "../../documents/resume.pdf";
+import Resume from "../../images/resume_ss.png";
+import { AccountBox, ContactMail, Twitter, Instagram, LinkedIn} from "@material-ui/icons";
 
 export default function contact() {
   return (
     <div className='contact' id='contact'>
-      <h1>Contact</h1>
-      <h2>Please reach out to me via:</h2>
-        <ul>
-          <li>Email: michaelphan35@gmail.com</li>
-          <li>PhoneL 480-745-0146</li>
-        </ul>
+      <div className="left">
+        <a href={PDF} target="_blank" rel="noreferrer"><h2>Resume:</h2></a>
+        <a href={PDF} target="_blank" rel="noreferrer"><img src={Resume} alt="" /></a>
+      </div>
+      <div className="center">
+        <h2>Please contact me using the following:</h2>
+      <div className="itemContainer">
+            <a href="/">
+              <AccountBox className="icon"/>
+              <span>+1 480 745 0146</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="mailto:michaelphan35@gmail.com">
+            <ContactMail className="icon"/>
+            <span>michaelphan35@gmail.com</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://twitter.com/Michael70506728" target="_blank" rel="noreferrer">
+              <Twitter className="icon"/>
+              <span>@Michael70506728</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://www.instagram.com/asiaticphan/" target="_blank" rel="noreferrer">
+              <Instagram className="icon"/>
+              <span>@asiaticphan</span>
+            </a>
+          </div>
+          <div className="itemContainer">
+            <a href="https://www.linkedin.com/in/michael-phan-b61903aa/" target="_blank" rel="noreferrer">
+              <LinkedIn className="icon"/>
+              <span>Michael Phan</span>
+            </a>
+          </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import "./navbar.scss"
-import { AccountBox, ContactMail } from "@material-ui/icons"
+
 
 export default function navbar({ menuOpen, setMenuOpen}) {
   return (
@@ -7,14 +7,6 @@ export default function navbar({ menuOpen, setMenuOpen}) {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Michael Phan</a>
-          <div className="itemContainer">
-            <AccountBox className="icon"/>
-            <span>+1 480 745 0146</span>
-          </div>
-          <div className="itemContainer">
-            <ContactMail className="icon"/>
-            <span>michaelphan35@gmail.com</span>
-          </div>
         </div>
         <div className="right">
           <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
